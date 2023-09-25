3a21c3b9b2b1e225b188cb5d6812d2d8c935e10f
xiaolinyin <xiaolinyin@bdgy.net>
2023-09-25 22:59:44 +0800

    Protect a worksheet

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -28,6 +28,32 @@ function action(event) {
   event.completed();
 }
 
+async function toggleProtection(args) {
+  try {
+    await Excel.run(async (context) => {
+      const sheet = context.workbook.worksheets.getActiveWorksheet();
+
+      sheet.load("protection/protected");
+      await context.sync();
+
+      if (sheet.protection.protected) {
+        sheet.protection.unprotect();
+      } else {
+        sheet.protection.protect();
+      }
+
+      await context.sync();
+    });
+  } catch (error) {
+    // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+    console.error(error);
+  }
+
+  args.completed();
+}
+
+Office.actions.associate("toggleProtection", toggleProtection);
+
 function getGlobal() {
   return typeof self !== "undefined"
     ? self
